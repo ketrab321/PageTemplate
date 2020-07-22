7f3606dd70899190cb209a96b730fb66f4fe095d
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-07-22 17:48:42 +0100

    Created card component + created fab menu

--- config.js
+++ config.js
@@ -4,7 +4,91 @@ const config = {
             title: "Strona Główna",
             message: "Tekst do tytułu 1",
             icon: "icon-home",
-            theme: "green"
+            theme: "green",
+            components: [
+                {
+                    id: 0,
+                    title: "Próba ala test",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+                    Proin tristique orci non nisi blandit, ut pretium orci auctor. \
+                    Mauris cursus non libero non porta. Sed id tortor faucibus, accumsan \
+                    dui ut, suscipit elit. Mauris vestibulum diam sed felis mollis imperdiet. \
+                    Sed bibendum commodo diam a congue. Curabitur sodales ultricies ante, ac \
+                    consectetur felis venenatis eu. Integer faucibus, arcu non dapibus hendrerit, \
+                    odio lorem accumsan neque, vitae egestas ex ante et leo. In porttitor, dolor \
+                    quis efficitur ultricies, sem justo condimentum mi, at dignissim sapien tortor\
+                     sed quam. Ut laoreet ac leo ut sagittis. Nullam dapibus nunc non faucibus \
+                     tristique. Phasellus mattis placerat urna a porta. In hac habitasse platea \
+                     dictumst. Donec nibh lectus, fermentum quis ultrices et, dictum iaculis elit. \
+                     Etiam suscipit imperdiet mi eget eleifend. Integer accumsan fringilla metus \
+                     vitae maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+                     Proin tristique orci non nisi blandit, ut pretium orci auctor. \
+                     Mauris cursus non libero non porta. Sed id tortor faucibus, accumsan \
+                     dui ut, suscipit elit. Mauris vestibulum diam sed felis mollis imperdiet. \
+                     Sed bibendum commodo diam a congue. Curabitur sodales ultricies ante, ac \
+                     consectetur felis venenatis eu. Integer faucibus, arcu non dapibus hendrerit, \
+                     odio lorem accumsan neque, vitae egestas ex ante et leo. In porttitor, dolor \
+                     quis efficitur ultricies, sem justo condimentum mi, at dignissim sapien tortor\
+                      sed quam. Ut laoreet ac leo ut sagittis. Nullam dapibus nunc non faucibus \
+                      tristique. Phasellus mattis placerat urna a porta. In hac habitasse platea \
+                      dictumst. Donec nibh lectus, fermentum quis ultrices et, dictum iaculis elit. \
+                      Etiam suscipit imperdiet mi eget eleifend. Integer accumsan fringilla metus \
+                      vitae maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+                      Proin tristique orci non nisi blandit, ut pretium orci auctor. \
+                      Mauris cursus non libero non porta. Sed id tortor faucibus, accumsan \
+                      dui ut, suscipit elit. Mauris vestibulum diam sed felis mollis imperdiet. \
+                      Sed bibendum commodo diam a congue. Curabitur sodales ultricies ante, ac \
+                      consectetur felis venenatis eu. Integer faucibus, arcu non dapibus hendrerit, \
+                      odio lorem accumsan neque, vitae egestas ex ante et leo. In porttitor, dolor \
+                      quis efficitur ultricies, sem justo condimentum mi, at dignissim sapien tortor\
+                       sed quam. Ut laoreet ac leo ut sagittis. Nullam dapibus nunc non faucibus \
+                       tristique. Phasellus mattis placerat urna a porta. In hac habitasse platea \
+                       dictumst. Donec nibh lectus, fermentum quis ultrices et, dictum iaculis elit. \
+                       Etiam suscipit imperdiet mi eget eleifend. Integer accumsan fringilla metus \
+                       vitae maximus."
+                },
+                {
+                    id: 1,
+                    title: "Próba ala test",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+                    Proin tristique orci non nisi blandit, ut pretium orci auctor. \
+                    Mauris cursus non libero non porta. Sed id tortor faucibus, accumsan \
+                    dui ut, suscipit elit. Mauris vestibulum diam sed felis mollis imperdiet. \
+                    Sed bibendum commodo diam a congue. Curabitur sodales ultricies ante, ac \
+                    consectetur felis venenatis eu. Integer faucibus, arcu non dapibus hendrerit, \
+                    odio lorem accumsan neque, vitae egestas ex ante et leo. In porttitor, dolor \
+                    quis efficitur ultricies, sem justo condimentum mi, at dignissim sapien tortor\
+                     sed quam. Ut laoreet ac leo ut sagittis. Nullam dapibus nunc non faucibus \
+                     tristique. Phasellus mattis placerat urna a porta. In hac habitasse platea \
+                     dictumst. Donec nibh lectus, fermentum quis ultrices et, dictum iaculis elit. \
+                     Etiam suscipit imperdiet mi eget eleifend. Integer accumsan fringilla metus \
+                     vitae maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+                     Proin tristique orci non nisi blandit, ut pretium orci auctor. \
+                     Mauris cursus non libero non porta. Sed id tortor faucibus, accumsan \
+                     dui ut, suscipit elit. Mauris vestibulum diam sed felis mollis imperdiet. \
+                     Sed bibendum commodo diam a congue. Curabitur sodales ultricies ante, ac \
+                     consectetur felis venenatis eu. Integer faucibus, arcu non dapibus hendrerit, \
+                     odio lorem accumsan neque, vitae egestas ex ante et leo. In porttitor, dolor \
+                     quis efficitur ultricies, sem justo condimentum mi, at dignissim sapien tortor\
+                      sed quam. Ut laoreet ac leo ut sagittis. Nullam dapibus nunc non faucibus \
+                      tristique. Phasellus mattis placerat urna a porta. In hac habitasse platea \
+                      dictumst. Donec nibh lectus, fermentum quis ultrices et, dictum iaculis elit. \
+                      Etiam suscipit imperdiet mi eget eleifend. Integer accumsan fringilla metus \
+                      vitae maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+                      Proin tristique orci non nisi blandit, ut pretium orci auctor. \
+                      Mauris cursus non libero non porta. Sed id tortor faucibus, accumsan \
+                      dui ut, suscipit elit. Mauris vestibulum diam sed felis mollis imperdiet. \
+                      Sed bibendum commodo diam a congue. Curabitur sodales ultricies ante, ac \
+                      consectetur felis venenatis eu. Integer faucibus, arcu non dapibus hendrerit, \
+                      odio lorem accumsan neque, vitae egestas ex ante et leo. In porttitor, dolor \
+                      quis efficitur ultricies, sem justo condimentum mi, at dignissim sapien tortor\
+                       sed quam. Ut laoreet ac leo ut sagittis. Nullam dapibus nunc non faucibus \
+                       tristique. Phasellus mattis placerat urna a porta. In hac habitasse platea \
+                       dictumst. Donec nibh lectus, fermentum quis ultrices et, dictum iaculis elit. \
+                       Etiam suscipit imperdiet mi eget eleifend. Integer accumsan fringilla metus \
+                       vitae maximus."
+                }
+            ]
         },
         {
             title: "Aktualności",
