b01e80455c08bf3ac0117ac131593b3b131d39b1
ketrab321 <37443550+ketrab321@users.noreply.github.com>
2020-07-23 18:02:34 +0100

    Added photos to card component and changed menu look

--- page/components.js
+++ page/components.js
@@ -9,15 +9,25 @@ Vue.component('card-component', {
         <div class="card w3-card w3-round-large w3-shadow w3-justify"
             :class="{'w3-hover-black': component.link != undefined }"
             >
-            <div class="card-title w3-center">
-                {{component.title}}
-                <div class="card-hr" :class="pageTheme(page)"></div>
-            </div>
-            
+                <div  class="card-side" v-if="component.side">
+                    <div>
+                        <div class="card-photo" v-if="component.side.photo">
+                            <img :src="component.side.photo"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-main" v-if="component.main">
+                    <div class="card-title w3-center">
+                        {{component.main.title}}
+                        <div class="card-hr" :class="pageTheme(page)"></div>
+                    </div>
+                    
 
-            <div class="card-text">
-             {{component.text}}
-             </div>
+                    <div class="card-text">
+                    {{component.main.text}}
+                    </div>
+                </div>
+
         </div>
     </a>
     `
